76e987e952aaf32ebc52e8a49c5eec1f16245e27
vcokltfre <vcokltfre@gmail.com>
2021-03-06 01:51:25 +0000

    feat: add basic server

---
+++ slashpatch/server.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+
+from discord_interactions import verify_key
+
+
+class Server(FastAPI):
+    def __init__(self, public_key: str, *args, interactions_path: str = "/interactions", **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.pubkey = public_key
+        self.add_route(interactions_path, self.command, methods=["POST"])
+
+        self.cogs = {}
+
+    async def command(self, req: Request):
+        body = await req.body()
+
+        sig = req.headers["X-Signature-Ed25519"]
+        ts = req.headers["X-Signature-Timestamp"]
+
+        if not verify_key(body, sig, ts, self.pubkey):
+            raise HTTPException(400)
+
+        return JSONResponse({
+            "type": 4,
+            "data": {
+                "content": "Ok."
+            }
+        })
