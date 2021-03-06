e80b52110aece60c4cc97fa885b5434d449776af
vcokltfre <vcokltfre@gmail.com>
2021-03-06 05:14:14 +0000

    feat: add resolved objects to context

--- slashpatch/context.py
+++ slashpatch/context.py
@@ -1,4 +1,7 @@
 from fastapi.responses import JSONResponse
+from typing import TypedDict
+
+from .objects import ResolvedUser, ResolvedMember, ResolvedChannel, ResolvedRole
 
 
 class Context:
@@ -6,12 +9,23 @@ class Context:
 
     def __init__(self, raw: dict):
         self.raw = raw
-        self.channel_id = raw["channel_id"]
+        self.channel_id = int(raw["channel_id"])
+        self.guild_id = int(raw.get("guild_id")) if "guild_id" in raw
+        self.interaction_id = int(raw["id"])
 
         self.data = data = self.raw["data"]
 
-        self.id = data["id"]
+        self.id = (data["id"])
         self.name = data["name"]
+
+        user = ResolvedUser(raw["member"]["user"])
+        self.author = ResolvedMember(user, raw["member"])
+
+        resolved = raw.get("resolved", {})
+        self._users: TypedDict[ResolvedUser] = {int(k): ResolvedUser(v) for k, v in resolved.get("users", {})}
+        self._members: TypedDict[ResolvedMember] = {int(k): ResolvedMember(self.users[int(k)], v) for k, v in resolved.get("members", {})}
+        self._channels: TypedDict[ResolvedChannel] = {int(k): ResolvedChannel(v) for k, v in resolved.get("channels", {})}
+        self._roles: TypedDict[ResolvedRole] = {int(k): ResolvedRole(v) for k, v in resolved.get("roles", {})}
 
     def respond(self, content: str = None, embed: dict = None, ephemeral: bool = False):
         if embed and ephemeral:
@@ -30,3 +44,15 @@ class Context:
             "type": 4,
             "data": data
         })
+
+    def get_user(self, id: str):
+        return self._users.get(id, None)
+
+    def get_member(self, id: str):
+        return self._members.get(id, None)
+
+    def get_channel(self, id: str):
+        return self._channels.get(id, None)
+
+    def get_role(self, id: str):
+        return self._roles.get(id, None)
